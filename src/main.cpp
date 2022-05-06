75ce5bf4ba996cad8e6ae3e370159365c935195a
youngermax <60242131+YoungerMax@users.noreply.github.com>
2022-05-05 21:19:55 -0400

    Fix if statement missing curly bracket

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@ int main() {
  
     if (DEBUG_LOGGING) {
         bot.on_log(dpp::utility::cout_logger());
+    }
  
     bot.on_ready([&bot](const dpp::ready_t& event) {
         std::cout << "Hello D++!" << std::endl;
